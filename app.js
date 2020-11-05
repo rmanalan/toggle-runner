7e10eea32ec2a68f90ede7e0f9468bf3958a451d
Rich Manalang <rmanalang@launchdarkly.com>
2020-11-05 14:10:32 -0800

    Lesson 2: Change log levels by flag

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
+require('dotenv').config()
+const LaunchDarkly = require('launchdarkly-node-server-sdk')
+ldClient = LaunchDarkly.init(process.env.LD_SDK_KEY)
+
 const express = require('express')
 const app = express()
 const pinoLogger = require('pino-http')({
   level: 'error',
 })
 const PORT = 3000
+const LD_USER = { key: 'toggle-runner-server' }
+
+// Updates logging level
+ldClient.on('update:logging-level', () => {
+  ldClient.variation('logging-level', LD_USER, 'info', (err, logLevel) => {
+    console.log(
+      `Logging level changed from ${pinoLogger.logger.level} to ${logLevel}`
+    )
+    pinoLogger.logger.level = logLevel
+  })
+})
 
 app.set('view engine', 'ejs')
 app.use(pinoLogger)
